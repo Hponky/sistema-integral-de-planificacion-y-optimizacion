c606a18955dbad13fae658f9c0c265de9c735d08
Humberto Agudelo <humberto228xd@gmail.com>
2025-09-16 16:27:44 -0500

    backend initial setup

---
+++ sipo/src/main/java/com/cc/sipo/SipoApplication.java
@@ -0,0 +1,13 @@
+package com.cc.sipo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SipoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SipoApplication.class, args);
+	}
+
+}
